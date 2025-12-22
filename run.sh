97da76fedc6c2f765257468a49f83ecb086e04cf
rfindra <if.indra.rf@gmail.com>
2025-12-22 17:41:42 +0700

    Update

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 source /home/indra/gemini-metadata/venv/bin/activate
 export PYTHONWARNINGS="ignore"
 cd /home/indra/gemini-metadata
-# Headless true mencegah error 'gio' di WSL
+# Headless true agar tidak error saat dijalankan via VBScript/Startup
 streamlit run app.py --server.headless true
